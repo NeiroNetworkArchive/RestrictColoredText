3c6c864c89203c779726edbd323d2470a5208064
banira4649 <banira.game@outlook.jp>
2022-04-10 22:27:48 +0900

    first commit

--- src/Main.php
+++ src/Main.php
@@ -2,10 +2,23 @@
 
 declare(strict_types=1);
 
-namespace NeiroNetwork\PluginTemplate;
+namespace NeiroNetwork\ChatColor;
 
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase{
-
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onPlayerChatEvent(PlayerChatEvent $event){
+		$player = $event->getPlayer();
+		if(!$this->getServer()->isOp($player->getName())){
+			$event->setMessage(TextFormat::clean($event->getMessage()));
+		}
+	}
 }
